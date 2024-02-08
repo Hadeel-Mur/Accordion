d67b3f2761decc994c9ec05567b3f051ff94d5a7
Hadeel Murshed <131537843+Hadeel-Mur@users.noreply.github.com>
2024-02-08 13:55:17 +0100

    single selection

--- accordion/src/components/accordion/index.jsx
+++ accordion/src/components/accordion/index.jsx
@@ -8,26 +8,35 @@ import data from './data';
 
 
 function Accordian() {
-  const [selected, setSelected] = useState(null)
+    const [selected, setSelected] = useState(null)
 
-  return (
-    <div className='wrapper'>
-        <div className='accordion'>
-         {data && data.length > 0 ? (
-            data.map(dataItem => (
-                <div className='item'>
-                    <div className='title'>
-                       <h3>{dataItem.question}</h3>
-                       <span>+</span>
-                    </div>
-                </div>
-            ))
-         ) : (
-            <div>No data found! </div>
-         )}
+    function handleSingleSelection(currentId) {
+        setSelected(currentId === selected ? null : currentId)
+
+    }
+    console.log(selected)
+    return (
+        <div className='wrapper'>
+            <div className='accordion'>
+                {data && data.length > 0 ? (
+                    data.map(dataItem => (
+                        <div className='item'>
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className='title'>
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {selected === dataItem.id ? (
+                                <div className='content'>{dataItem.answer}</div>
+                            )
+                                : null}
+                        </div>
+                    ))
+                ) : (
+                    <div>No data found! </div>
+                )}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Accordian;
